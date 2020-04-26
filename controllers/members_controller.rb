561b77f966bce00e163f003e26a1ede9ce5d92a1
Alan <alanhood77@gmail.com>
2020-04-26 21:52:14 +0100

    membership input changed to option selector and form fields given required atribute

--- controllers/members_controller.rb
+++ controllers/members_controller.rb
@@ -3,22 +3,23 @@ require( 'sinatra/contrib/all' )
 require_relative( '../models/member.rb' )
 also_reload( '../models/*' )
 
-get '/members' do
+get '/members' do #index
     @members = Member.all()
     erb ( :"members/index" )
 end
 
-get '/members/new' do
+get '/members/new' do #new
     erb ( :"members/new" )
 end
 
-get '/members/:id' do
+get '/members/:id' do #show
     @member = Member.find( params[:id] )
     erb ( :"members/show" )
 end
 
-post '/members' do
+post '/members' do #create
     @member = Member.new(params)
     @member.save()
     erb ( :"members/create" )
 end
+
