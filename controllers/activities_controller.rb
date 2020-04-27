91cb437a3327a2a2da2c910c9609412233450122
Alan <alanhood77@gmail.com>
2020-04-27 08:11:48 +0100

    craete, index, new and show added to activity controller and view

--- controllers/activities_controller.rb
+++ controllers/activities_controller.rb
@@ -1,10 +1,18 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
-
 require_relative( '../models/activity.rb' )
 also_reload( '../models/*')
 
-get '/activities' do
+get '/activities' do #index
     @activities = Activity.all()
-    erb( :index )
+    erb ( :"activities/index" )
+end
+
+get '/activities/new' do #new
+    erb ( :"activities/new" )
+end
+
+get '/activities/:id' do #show
+    @activity = Activity.find( params[:id] )
+    erb ( :"activities/show" )
 end
